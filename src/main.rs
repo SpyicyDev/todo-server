95fdcdb4eea90070bce758819f3196181b0c8b74
Mack Haymond <mack.haymond@icloud.com>
2023-03-19 01:00:53 -0400

    oh ma god i hate cors

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 mod sql;
 
-use actix_cors::Cors;
 use crate::sql::*;
 
 use actix_web::*;
@@ -9,7 +8,7 @@ use actix_web::*;
 async fn get_all() -> impl Responder {
     let rows = get_all_tasks().await.unwrap();
     let serialized = serde_json::to_string(&rows).unwrap();
-    HttpResponse::Ok().insert_header(("Access-Control-Allow-Origin","")).body(serialized)
+    HttpResponse::Ok().insert_header(("Access-Control-Allow-Origin","*")).body(serialized)
 }
 
 #[get("/add-todo/{id}/{text}")]
@@ -17,7 +16,7 @@ async fn add(path: web::Path<(i32, String)>) -> impl Responder {
     let (id, text) = path.into_inner();
     add_task(id, &text).await;
     let res = format!("Added todo with ID {id} and text {text}");
-    HttpResponse::Ok().insert_header(("Access-Control-Allow-Origin","")).body(res)
+    HttpResponse::Ok().insert_header(("Access-Control-Allow-Origin","*")).body(res)
 }
 
 #[get("/delete-todo/{id}")]
@@ -25,21 +24,21 @@ async fn delete(path: web::Path<i32>) -> impl Responder {
     let id = path.into_inner();
     delete_task(id).await;
     let res = format!("Deleted todo with ID {id}");
-    HttpResponse::Ok().insert_header(("Access-Control-Allow-Origin","")).body(res)
+    HttpResponse::Ok().insert_header(("Access-Control-Allow-Origin","*")).body(res)
 }
 
 #[get("/get-count")]
 async fn get_count_handler() -> impl Responder {
     let count = get_count().await;
     let serialized = serde_json::to_string(&count).unwrap();
-    HttpResponse::Ok().insert_header(("Access-Control-Allow-Origin","")).body(serialized)
+    HttpResponse::Ok().insert_header(("Access-Control-Allow-Origin","*")).body(serialized)
 }
 
 #[get("/inc-count")]
 async fn inc_count_handler() -> impl Responder {
     inc_count().await;
 
-    HttpResponse::Ok().insert_header(("Access-Control-Allow-Origin","")).body("Successfully incremented id!")
+    HttpResponse::Ok().insert_header(("Access-Control-Allow-Origin","*")).body("Successfully incremented id!")
 }
 
 #[actix_web::main]
