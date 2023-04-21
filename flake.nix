f59318376e4ff78280c5c0756261f44f6a999d73
Mack Haymond <mack.haymond@icloud.com>
2023-04-21 18:14:55 -0400

    updated a bit

--- flake.nix
+++ flake.nix
@@ -1,7 +1,16 @@
 {
   description = "A very basic flake";
+  
+  inputs = {
+    nixpkgs.url = "rust-overlay/nixpkgs";
 
-  outputs = { self, nixpkgs }: {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, inputs }: with inputs; {
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
