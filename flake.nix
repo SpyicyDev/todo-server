fcf60e5bf2f42109d20afadde03b58a2057b3541
Mack Haymond <mack.haymond@icloud.com>
2023-04-23 17:14:54 -0400

    this time

--- flake.nix
+++ flake.nix
@@ -36,8 +36,8 @@
       in rec {
         packages.rustPackage-x86_64-linux = naersk'.buildPackage {
           src = ./.;
-          nativeBuildInputs = [ pkgs.pkg-config pkgs.zstd pkgs.openssl.dev pkgs.pkgsStatic.stdenv.cc ];
-          buildInputs = [ pkgs.openssl ];
+          nativeBuildInputs = [ pkgs.pkg-config pkgs.zstd pkgs.openssl.dev ];
+          buildInputs = [ pkgs.openssl pkgs.stdenv.cc ];
           CARGO_BUILD_TARGET = "x86_64-unknown-linux-gnu";
         };
 
