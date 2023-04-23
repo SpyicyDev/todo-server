9cf92c884035ae7eb41ebbdb2074f9fd21180ec6
Mack Haymond <mack.haymond@icloud.com>
2023-04-23 13:45:32 -0400

    omg im so stupid again

--- flake.nix
+++ flake.nix
@@ -22,8 +22,7 @@
 
         toolchain = with fenix.packages.${system};
           combine [
-            minimal.rustc
-            minimal.cargo
+            defaultToolchain
             targets.x86_64-unknown-linux-musl.latest.rust-std
             targets.x86_64-pc-windows-gnu.latest.rust-std
             targets.i686-pc-windows-gnu.latest.rust-std
