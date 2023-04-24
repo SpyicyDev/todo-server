f1f91217d197407d7767aab96d2ac8ae0a336dd0
Mack Haymond <mack.haymond@icloud.com>
2023-04-24 07:43:14 -0400

    adding docker to nixddddddd

--- flake.nix
+++ flake.nix
@@ -40,7 +40,7 @@
           nativeBuildInputs = [ pkgs.pkg-config pkgs.pkgsCross.musl64.stdenv.cc.cc ];
           buildInputs = [ pkgs.openssl pkgs.openssl.dev ];
           CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
-          TARGET_CC = "x86_64-unknown-linux-musl-gcc";
+          TARGET_CC = "x86_64-unknown-linux-musdl-gcc";
         };
 
         packages.dockerImage = pkgs.dockerTools.buildImage {
