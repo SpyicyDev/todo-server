0117ddde164f3a6a223fed2d31c1cf4e1e9b40cb
Mack Haymond <mack.haymond@icloud.com>
2023-04-24 22:32:48 -0400

    this time

--- rustbuild.nix
+++ rustbuild.nix
@@ -1,8 +1,6 @@
 {naersk', pkgs }: naersk'.buildPackage {
   src = ./.;
   doCheck = true;
-  nativeBuildInputs = [ pkgs.pkg-config pkgs.pkgsStatic.stdenv.cc ];
+  nativeBuildInputs = [ pkgs.pkg-config ];
   buildInputs = [ pkgs.openssl pkgs.openssl.dev ];
-  CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
-  CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
 }
