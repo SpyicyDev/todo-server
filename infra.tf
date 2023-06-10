631c62550c9bf21a1d22d0386c1f7727582a90c9
Mack Haymond <mack.haymond@icloud.com>
2023-06-10 18:39:07 -0400

    added remote state

--- infra.tf
+++ infra.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "http" {
+    address        = "https://api.tfstate.dev/github/v1"
+    lock_address   = "https://api.tfstate.dev/github/v1/lock"
+    unlock_address = "https://api.tfstate.dev/github/v1/lock"
+    lock_method    = "PUT"
+    unlock_method  = "DELETE"
+    username       = "spyicydev/todo-server"
+  }
   required_providers {
     digitalocean  = {
       source = "digitalocean/digitalocean"
